04f2705544348da14bac50000147aef7c566586b
WarriorIng64 <christhehorton@gmail.com>
2013-10-13 19:43:23 -0400

    Improve window drawing performance.

--- GxSubOS.py
+++ GxSubOS.py
@@ -12,6 +12,7 @@ mouse_x, mouse_y = 0, 0
 
 pygame.display.set_caption("GxSubOS 2.0 Garter Snake")
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+desktop_surface = pygame.Surface((screen.get_width(), screen.get_height()))
 
 system_font = pygame.font.Font(None, 12)
 
@@ -45,6 +46,25 @@ def DrawWindowShadow(screen, window_rect):
   screen.blit(pygame.transform.scale(shadow_r_image, (shadow_width, window_rect.height - shadow_width)), [window_rect.right - shadow_offset, window_rect.top + shadow_offset, shadow_width, window_rect.height - shadow_width])
   screen.blit(pygame.transform.scale(shadow_b_image, (window_rect.width - shadow_width, shadow_width)), [window_rect.left + shadow_offset, window_rect.bottom - shadow_offset, window_rect.width - shadow_width, shadow_width])
 
+def DrawDesktopSurface(window_list):
+  # Update the surface behind the focused window
+  desktop_surface.blit(wallpaper, wallpaper_rect)
+  for window in window_list[:-1]:
+    window.redraw(desktop_surface)
+    if not window.is_maximized:
+      DrawWindowShadow(desktop_surface, window.rect)
+    desktop_surface.blit(window.surface, window.rect)
+
+def DrawTopWindow(surface, window_list):
+  # Draws the top window onto the desktop_surface
+  if len(window_list) < 1:
+    return
+  window = window_list[-1]
+  window.redraw(surface)
+  if not window.is_maximized:
+    DrawWindowShadow(surface, window.rect)
+  surface.blit(window.surface, window.rect)
+
 launcher = Launcher(width, height)
 window_list = []
 launcher_list = []
@@ -53,9 +73,12 @@ CreateWindow(200, 200, 500, 250, "Window 2")
 CreateWindow(300, 100, 600, 400, "Window 3")
 startbutton = Startbutton()
 
+DrawDesktopSurface(window_list)
+
 # MAIN LOOP
 while 1:
   # Check if we quit yet and handle events for windows
+  redraw_all_windows = False
   mouse_button = 0
   mouse_event = None;
   for event in pygame.event.get():
@@ -78,6 +101,7 @@ while 1:
       window.update(mouse_event, mouse_button)
       if window.window_closed:
         window_list.remove(window)
+        redraw_all_windows = True
     # Determine which window gets focus
     if event.type == pygame.MOUSEBUTTONDOWN:
       focused_window_found = False
@@ -96,23 +120,22 @@ while 1:
         launcher_list.remove(button)
   
   # Maintain proper window order
-  for window in window_list:
+  for window in window_list[:-1]:
     if window.has_focus:
       # This is always last in the window_list so it's drawn on top
       focused_window = window
       window_list.remove(window)
       window_list.append(focused_window)
+      redraw_all_windows = True
   # Update launcher button positions
   for button in launcher_list:
     button.update_position()
   
   # Drawing and game object updates
-  screen.blit(wallpaper, wallpaper_rect)
-  for window in window_list:
-    window.redraw(screen)
-    if not window.is_maximized:
-      DrawWindowShadow(screen, window.rect)
-    screen.blit(window.surface, window.rect)
+  if redraw_all_windows:
+    DrawDesktopSurface(window_list)
+  screen.blit(desktop_surface, desktop_surface.get_rect())
+  DrawTopWindow(screen, window_list)
   launcher.update(screen)
   startbutton.update(mouse_event, mouse_button)
   screen.blit(launcher.launcher_surface, (0, 0))
