e9c51f3684b2d721abf3c098fc274f488fa325fb
WarriorIng64 <christhehorton@gmail.com>
2013-10-19 17:11:43 -0400

    Add GPL v3 copyright notice to individual files.

--- wallpaper.py
+++ wallpaper.py
@@ -1,3 +1,19 @@
+# This file is part of GxSubOS.
+# Copyright (C) 2013 Christopher Kyle Horton <christhehorton@gmail.com>
+
+# GxSubOS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# GxSubOS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GxSubOS. If not, see <http://www.gnu.org/licenses/>.
+
 import sys, pygame
 
 default_path = "graphics/default_wallpaper.png"
