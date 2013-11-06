2d0609c42aaa25fff4839a63f81cfa4cf2dc80b3
Christopher Kyle Horton <christhehorton@gmail.com>
2013-11-06 11:29:46 -0500

    Display wallpaper previews, add key debugging code.

--- wallpaperswitcher.py
+++ wallpaperswitcher.py
@@ -32,20 +32,23 @@ class WallpaperSwitcher:
     self.current_selection = 0
     self.closed = False
     self.surface = pygame.Surface((w / 3, h), pygame.SRCALPHA)
-    self.rect = pygame.Rect((w * 2 / 3, 0), (w / 3, h))
+    self.rect = pygame.Rect((w * 2 / 3, 0), (w / 3 + 2, h))
     self.background_surface = pygame.Surface((w / 3, h), pygame.SRCALPHA)
     self.pane_surface = pygame.Surface((w / 3, h), pygame.SRCALPHA)
     self.pane_surface.fill(switcher_color)
 
     self.preview_size = (w / 3 - 2 * padding, h / 3 - 2 * padding)
 
+    self.UpdatePreviewList()
+
   def SetWallpaper(self, wp):
     self.wallpaper = wp
 
   def UpdatePreviewList(self):
     for i in range(self.wallpaper.GetNumWallpapers()):
-      self.preview_list.append(self.wallpaper.GetWallpaperPreview(i))
-      self.preview_list_rects.append(pygame.Rect((padding, padding + (padding * self.preview_size[1]) * i), self.preview_size))
+      preview_rect = pygame.Rect((padding, padding + (padding + self.preview_size[1]) * i), self.preview_size)
+      self.preview_list.append(self.wallpaper.GetWallpaperPreview(i, preview_rect))
+      self.preview_list_rects.append(preview_rect)
 
   def IncrementCurrentSelection(self):
     self.current_selection += 1
@@ -61,10 +64,13 @@ class WallpaperSwitcher:
     # Wallpaper scrolling and selection
     if event.key is pygame.K_UP:
       self.IncrementCurrentSelection()
+      print "Current selection on K_UP: " + str(self.current_selection)
     elif event.key is pygame.K_DOWN:
       self.DecrementCurrentSelection()
+      print "Current selection on K_DOWN: " + str(self.current_selection)
     elif event.key is pygame.K_RETURN:
       self.wallpaper.SwitchToWallpaperInList(self.current_selection)
+      print "Switched to wallpaper with index = " + str(self.current_selection)
       self.closed = True
     elif event.key is pygame.K_ESCAPE:
       self.closed = True
