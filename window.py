1f0dd5c8840a4a1765db7955b500836a9cf03f45
WarriorIng64 <christhehorton@gmail.com>
2013-11-06 17:27:14 -0500

    Add documentation strings to the window module.

--- window.py
+++ window.py
@@ -23,6 +23,7 @@ titlebar_height = 24
 titlebar_font = pygame.font.Font("fonts/Roboto/Roboto-Regular.ttf", 18)
 
 class Window:
+  """A class for the application windows in the desktop environment of GxSubOS."""
   window_color = glass.glass_color
   window_color_opaque = glass.glass_color
   window_color.a = glass.glass_alpha
@@ -53,6 +54,7 @@ class Window:
     self.top_level_container = container.Container(None, self)
   
   def Redraw(self, screen, blurred_surface=None):
+    """Redraws this Window's surface in preparation for the next blitting."""
     if glass.enable_transparency:
       if glass.enable_blur and blurred_surface != None:
         self.background_surface.blit(blurred_surface, blurred_surface.get_rect().move(-self.rect.x, -self.rect.y))
@@ -65,6 +67,8 @@ class Window:
     self.surface.blit(self.window_surface, [0, 0, 0, 0])
   
   def DrawTitlebarSeparator(self, surface, upper):
+    """Draws a titlebar separator on the given surface. upper is a Boolean
+    indicating whether this is for the top titlebar."""
     if upper:
       height = titlebar_height
     else:
@@ -78,6 +82,7 @@ class Window:
     self.window_surface.blit(separator_surface, [0, 0, 0, 0])
   
   def DrawTitlebar(self):
+    """Draw the titlebars on the window_surface."""
     # Draw titlebar background
     pygame.draw.rect(self.window_surface, glass.glass_color, [0, 0, self.rect.width, titlebar_height])
     pygame.draw.rect(self.window_surface, glass.glass_color, [0, self.rect.height - titlebar_height, self.rect.width, titlebar_height])
@@ -90,16 +95,21 @@ class Window:
     self.window_surface.blit(text_surf, [titlebar_height, 1, 0, 0])
   
   def MoveByAmount(self, x, y):
+    """Moves this Window by the given x and y values."""
     self.rect.move_ip(x, y)
     self.restore_rect = self.rect
   
   def CloseButtonClicked(self, x, y):
+    """Returns true if the given x and y values are inside the area where the
+    close button is."""
     close_x1, close_x2 = self.rect.left, self.rect.left + titlebar_height
     close_y1, close_y2 = self.rect.top, self.rect.top + titlebar_height
     return close_x1 < x < close_x2 and close_y1 < y < close_y2
   
   def ResizeButtonClicked(self, x, y):
-    # Do not resize maximized windows
+    """Returns true if the given x and y values are inside the area where the
+    resize button is. This function will always return false if the window is
+    maximized."""
     if self.is_maximized:
       return False
     resize_x1 = self.rect.x + self.rect.width - titlebar_height
@@ -109,6 +119,8 @@ class Window:
     return resize_x1 < x < resize_x2 and resize_y1 < y < resize_y2
   
   def TitlebarClicked(self, x, y):
+    """Returns true if the given x and y values are inside the area where the
+    titlebars are."""
     x1, x2 = self.rect.left, self.rect.right
     y1, y2 = self.rect.top, self.rect.top + titlebar_height
     top_titlebar_clicked = x1 < x < x2 and y1 < y < y2
@@ -117,11 +129,14 @@ class Window:
     return top_titlebar_clicked or bottom_titlebar_clicked
   
   def WindowClicked(self, x, y):
+    """Returns true if the given x and y values are inside the area of this
+    Window."""
     x1, x2 = self.rect.left, self.rect.right
     y1, y2 = self.rect.top, self.rect.bottom
     return x1 < x < x2 and y1 < y < y2
   
   def DrawWindowSurface(self):
+    """Updates the window surface, but not the background or anything else."""
     # Draw the window chrome and prepare the window_surface for blitting
     w, h = self.rect.width, self.rect.height
     window_rect = self.window_surface.get_rect()
@@ -134,6 +149,7 @@ class Window:
     self.window_surface.convert_alpha()
   
   def SetFocus(self, focus):
+    """Sets the focused state of this Window."""
     if self.has_focus != focus:
       self.has_focus = focus
       self.DrawWindowSurface()
@@ -141,6 +157,7 @@ class Window:
       self.has_focus = focus
   
   def Resize(self, new_width, new_height):
+    """Resizes this Window to the new width and height values provided."""
     correct_width = max(new_width, titlebar_height * 2)
     correct_height = max(new_height, titlebar_height * 2)
     x, y = self.rect.x, self.rect.y
@@ -159,11 +176,14 @@ class Window:
     self.content_area_rect = pygame.Rect(0, t_h, w, h - 2 * t_h)
   
   def CreateSurfaces(self, w, h):
+    """Creates the surfaces this Window will use for its different drawing
+    layers."""
     self.surface = pygame.Surface((w, h), pygame.SRCALPHA)
     self.window_surface = pygame.Surface((w, h), pygame.SRCALPHA)
     self.background_surface = pygame.Surface((w, h), pygame.SRCALPHA)
   
   def Maximize(self):
+    """Maximizes this Window."""
     self.restore_rect = self.rect
     maximized_width = pygame.display.Info().current_w - 48
     maximized_height = pygame.display.Info().current_h
@@ -174,6 +194,7 @@ class Window:
     self.is_maximized = True
   
   def Unmaximize(self):
+    """Unmaximizes this Window."""
     if self.is_maximized:
       self.rect.x = self.restore_rect.x
       self.rect.y = self.restore_rect.y
@@ -182,6 +203,8 @@ class Window:
       self.is_maximized = False
   
   def Drag(self, mouse_x, mouse_y):
+    """Drags this Window using the given mouse coordinates and comparing them
+    to internally stored coordinates where the initial click occurred."""
     offset_x, offset_y = mouse_x - self.click_x, mouse_y - self.click_y
     self.MoveByAmount(offset_x, offset_y)
     # Dragging limits
@@ -193,17 +216,18 @@ class Window:
     self.click_y = mouse_y
   
   def MouseResize(self, mouse_x, mouse_y):
+    """Resizes this window using the given mouse coordinates."""
     offset_x, offset_y = mouse_x - self.click_x, mouse_y - self.click_y
     self.Resize(self.rect.width + offset_x, self.rect.height + offset_y)
     self.click_x = mouse_x
     self.click_y = mouse_y
   
   def StartResizing(self, mouse_x, mouse_y):
-    # Puts this window into resize mode
+    """Puts this Window into resize mode."""
     self.being_resized = True
     self.click_x, self.click_y = mouse_x, mouse_y
   
   def StartDragging(self, mouse_x, mouse_y):
-    # Puts this window into dragging mode
+    """"Puts this Window into dragging mode."""
     self.being_dragged = True
     self.click_x, self.click_y = mouse_x, mouse_y
