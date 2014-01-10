f31bdce747b1b9d055dcf52b8640f75498dfc736
WarriorIng64 <christhehorton@gmail.com>
2014-01-10 16:34:21 -0500

    Actually clone default app repos.

--- appdb.py
+++ appdb.py
@@ -20,19 +20,17 @@ import MySQLdb as mdb
 # This module was constructed with the help of this online tutorial:
 # http://zetcode.com/db/mysqlpython/
 
-con = mdb.connect('localhost', 'localuser', 'gxappdbcon', 'gxappdb')
+con = mdb.connect('localhost', 'GxSubOSUser', 'GxSubOSPassword', 'GxSubOS')
 
 class AppDB:
   def __init__(self):
     with con:
       cur = con.cursor()
-      cur.execute("DROP DATABASE IF EXISTS GxSubOS")
-      cur.execute("CREATE DATABASE GxSubOS")
       cur.execute("USE GxSubOS")
       cur.execute("DROP TABLE IF EXISTS Apps")
       cur.execute("CREATE TABLE Apps(AppId INT PRIMARY KEY AUTO_INCREMENT, \
                    AppName VARCHAR(20), \
-                   Default BOOLEAN, \
+                   DefaultApp BOOLEAN, \
                    DirName VARCHAR(20), \
                    CurVersion VARCHAR(3), \
                    UpdateVersion VARCHAR(3), \
@@ -42,24 +40,24 @@ class AppDB:
   def RetrieveAppNames(self):
     '''Retrieves a list of the names of all apps in the database.'''
     appslist = []
-    cur = con.cursor()
+    cur = con.cursor(mdb.cursors.DictCursor)
     cur.execute("SELECT AppName FROM Apps")
     rows = cur.fetchall()
     for row in rows:
-      appslist.append(rows[1])
+      appslist.append(row["AppName"])
     return appslist
   
   def InsertDefaultApps(self):
     '''Inserts the info for the default apps into the database.'''
     cur = con.cursor()
     # The default apps are currently hard-coded here
-    fields = "AppName,Default,DirName,CurVersion,UpdateVersion,WebsiteUrl,RepoURL"
-    values = "'GxCalculator',True,'GxCalculator','0.1','0.1','https://github.com/WarriorIng64/GxCalculator','https://github.com/WarriorIng64/GxCalculator.git'"
+    fields = "AppName,DefaultApp,DirName,CurVersion,UpdateVersion,WebsiteUrl,RepoURL"
+    values = "'GxCalculator',true,'GxCalculator','0.1','0.1','https://github.com/WarriorIng64/GxCalculator','https://github.com/WarriorIng64/GxCalculator.git'"
     cur.execute("INSERT INTO Apps(" + fields + ") VALUES(" + values + ")")
   
   def GetAppInfo(self, appname):
     '''Gets the info for the given app name as a dictionary.'''
     cur = con.cursor(mdb.cursors.DictCursor)
-    cur.execute("SELECT * FROM Apps WHERE AppName = " + appname)
+    cur.execute("SELECT * FROM Apps WHERE AppName = '" + appname + "'")
     rows = cur.fetchall()
     return rows[0]
--- setup.py
+++ setup.py
@@ -15,21 +15,27 @@
 # along with GxSubOS. If not, see <http://www.gnu.org/licenses/>.
 
 import os, git
-from appdb import AppDb
+from appdb import AppDB
 
 def Setup():
   # Sets up the necessary data for the SubOS if this is the first run.
-  if(not os.path.isdir("apps")):
+  if not os.path.isdir(os.getcwd() + "/apps"):
+    print "First run; setting up app database."
     # App database setup
     database = AppDB()
     database.InsertDefaultApps()
+    initialwd = os.getcwd()
     # Create directories and retrieve default apps from Git repos
-    os.makedirs("apps")
-    os.makedirs("default")
+    #os.makedirs(os.getcwd() + "/apps")
+    os.makedirs(os.getcwd() + "/apps/default")
+    os.chdir(os.getcwd() + "/apps/default")
+    appswd = os.getcwd()
     applist = database.RetrieveAppNames()
     for appname in applist:
       # Create each repo and pull
+      os.chdir(appswd)
       appinfo = database.GetAppInfo(appname)
-      appdir = "apps/default/" + appinfo["DirName"]
-      os.makedirs(appdir)
-      git.Repo.clone_from(appinfo["RepoUrl"], appdir)
+      #appdir = appinfo["DirName"]
+      #os.makedirs(appdir)
+      #os.chdir(appdir)
+      os.system("git clone " + appinfo["RepoUrl"])
