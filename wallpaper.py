742fb687e8bfe65ec6e9cd23df435197e84bde39
Christopher Kyle Horton <christhehorton@gmail.com>
2014-02-04 19:11:55 -0500

    Update copyright.

--- wallpaper.py
+++ wallpaper.py
@@ -1,5 +1,5 @@
 # This file is part of GxSubOS.
-# Copyright (C) 2013 Christopher Kyle Horton <christhehorton@gmail.com>
+# Copyright (C) 2014 Christopher Kyle Horton <christhehorton@gmail.com>
 
 # GxSubOS is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
